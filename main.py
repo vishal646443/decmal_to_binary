97e371553c522489f661f647034744c2e3d29393
vishal <vivevich3@gmail.com>
2023-02-01 19:39:50 +0530

    Initial commit

---
+++ main.py
@@ -0,0 +1,16 @@
+def Fibonacci(number):
+    if (number == 0):
+        return 0
+    elif (number == 1):
+        return 1
+    else:
+        return (Fibonacci(number - 2) + Fibonacci(number - 1))
+while True:
+    number = 10
+    for n in range(0, number):
+        list = ["vis0", "drrr1", "scribi*2", "boom3", "boom4", "boom5", "boom6", "yes7", "yes8", "yes9", "yes10"]
+        list1=[]
+        list1.append(Fibonacci(n))
+        print(list1)
+        for i in range():
+            print(i)
